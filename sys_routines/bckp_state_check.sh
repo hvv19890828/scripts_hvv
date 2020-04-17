79b7fa759be3db7375f6092e1874ccd8c5227710
Viacheslav Hudzovskyi <hvv19890828@gmail.com>
2020-04-17 06:26:47 +0300

    bulk5

--- sys_routines/bckp_state_check.sh
+++ sys_routines/bckp_state_check.sh
@@ -7,10 +7,6 @@ if (( $stateofsdb1 == 1 ));
 then
 mount /dev/sdb1 /bckp_hvv/
 chmod -R 777 /bckp_hvv
-#refresh_network.sh related (reset of the indevidual network interfaces counters)
-echo 0 > /scripts_hvv/sys_routines/if_tries_count100
-echo 0 > /scripts_hvv/sys_routines/if_tries_count101
-#refresh_network.sh related (reset of the individual network interfaces counters)
 else
 sleep 20
 /scripts_hvv/sys_routines/gracefull_system_restart.sh
