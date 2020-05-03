413399c23b21cb054334e9802172f5fe21d9559b
Viacheslav Hudzovskyi <hvv19890828@gmail.com>
2020-05-03 06:31:07 +0300

    activated auto-refresf for nif101

--- sys_routines/refresh_network.sh
+++ sys_routines/refresh_network.sh
@@ -2,38 +2,38 @@
 
 #vars initialization
 
-#stateof101=$(curl --interface wlx502b73d80f39 -m 1 192.168.0.1:55099 | grep -i archer | wc -l)
+stateof101=$(curl --interface enx000ec79ecb52 -m 1 192.168.0.1:55099 | grep -i archer | wc -l)
 stateof100=$(curl --interface wlp2s0 -m 1 192.168.0.1:55099 | grep -i archer | wc -l)
-#if (( $stateof100 > 0 )); #&& $stateof101 > 0
-#then
-#echo 0 > /scripts_hvv/sys_routines/if_tries_count
-#fi
+if (( $stateof100 > 0 && $stateof101 > 0 )); #&& $stateof101 > 0
+then
+echo 0 > /scripts_hvv/sys_routines/if_tries_count
+fi
 if (( $stateof100 > 0 ));
 then
 echo 0 > /scripts_hvv/sys_routines/if_tries_count100
 fi
-#if (( $stateof101 > 0 ));
-#then
-#echo 0 > /scripts_hvv/sys_routines/if_tries_count101
-#fi
-#if_tries_counter=$(cat /scripts_hvv/sys_routines/if_tries_count)
+if (( $stateof101 > 0 ));
+then
+echo 0 > /scripts_hvv/sys_routines/if_tries_count101
+fi
+if_tries_counter=$(cat /scripts_hvv/sys_routines/if_tries_count)
 if_tries_counter100=$(cat /scripts_hvv/sys_routines/if_tries_count100)
-#if_tries_counter101=$(cat /scripts_hvv/sys_routines/if_tries_count101)
+if_tries_counter101=$(cat /scripts_hvv/sys_routines/if_tries_count101)
 
 #service connection refresh
 
-if (( $stateof100 < 1 )); #&& $if_tries_counter < 3
+if (( $stateof100 < 1 && $if_tries_counter < 3 )); #&& $if_tries_counter < 3
 then
-if (( $if_tries_counter100 < 6 ));
+if (( $if_tries_counter100 >= 4 ));
 then
-#let "if_tries_counter=if_tries_counter+1"
-#echo $if_tries_counter > /scripts_hvv/sys_routines/if_tries_count
+let "if_tries_counter=if_tries_counter+1"
+echo $if_tries_counter > /scripts_hvv/sys_routines/if_tries_count
 #reset of the indevidual network interfaces counters
-#echo 0 > /scripts_hvv/sys_routines/if_tries_count100
-#echo 0 > /scripts_hvv/sys_routines/if_tries_count101
+echo 0 > /scripts_hvv/sys_routines/if_tries_count100
+echo 0 > /scripts_hvv/sys_routines/if_tries_count101
 #reset of the individual network interfaces counters
-#/scripts_hvv/sys_routines/gracefull_system_restart.sh
-#else
+/scripts_hvv/sys_routines/gracefull_system_restart.sh
+else
 ip link set wlp2s0 down
 sleep 2
 modprobe -r iwlwifi
@@ -47,7 +47,7 @@ sleep 2
 modprobe iwlwifi
 sleep 2
 ip link set wlp2s0 up
-sleep 4
+sleep 5
 #
 vvmstat=$(vagrant global-status | grep node | wc -l)
 let "vvmstat=vvmstat+1"
@@ -77,41 +77,40 @@ fi
 
 #data connection refresh
 
-#if (( $stateof101 < 1 && $if_tries_counter < 3 ));
-#then
-#if (( $if_tries_counter101 >= 2 ));
-#then
-#let "if_tries_counter=if_tries_counter+1"
-#echo $if_tries_counter > /scripts_hvv/sys_routines/if_tries_count
+if (( $stateof101 < 1 && $if_tries_counter < 3 ));
+then
+if (( $if_tries_counter101 >= 4 ));
+then
+let "if_tries_counter=if_tries_counter+1"
+echo $if_tries_counter > /scripts_hvv/sys_routines/if_tries_count
 #reset of the indevidual network interfaces counters
-#echo 0 > /scripts_hvv/sys_routines/if_tries_count100
-#echo 0 > /scripts_hvv/sys_routines/if_tries_count101
+echo 0 > /scripts_hvv/sys_routines/if_tries_count100
+echo 0 > /scripts_hvv/sys_routines/if_tries_count101
 #reset of the individual network interfaces counters
-#/scripts_hvv/sys_routines/gracefull_system_restart.sh
-#else
+/scripts_hvv/sys_routines/gracefull_system_restart.sh
+else
+ip link set enx000ec79ecb52 down
 #ip link set wlx502b73d80f39 down
 #ip link set wlx00e0262e336b down
-#sleep 2
+sleep 2
+modprobe -r ax88179_178a
 #modprobe -r 8812au
 #modprobe -r rtl8188fu
-#chmod -R 777 /sys/bus/pci
-#chmod -R 777 /sys/bus/pci/devices/0000:00:14.0
-#sleep 2
-#echo 1 > /sys/bus/pci/devices/0000:00:14.0/remove
-#sleep 2
-#echo 1 > /sys/bus/pci/rescan
-#sleep 2
+chmod -R 777 /sys/bus/pci
+chmod -R 777 /sys/bus/pci/devices/0000:00:14.0
+sleep 2
+echo 1 > /sys/bus/pci/devices/0000:00:14.0/remove
+sleep 2
+echo 1 > /sys/bus/pci/rescan
+sleep 2
+modprobe ax88179_178a
 #modprobe 8812au
 #modprobe rtl8188fu
-#sleep 2
+sleep 2
+ip link set enx000ec79ecb52 up
 #ip link set wlx502b73d80f39 up
 #ip link set wlx00e0262e336b up
-#sleep 5
-#cd /scripts_hvv/vmprov/vagrant/
-#vagrant suspend
-#sleep 2
-#vagrant resume
-#let "if_tries_counter101=if_tries_counter101+1"
-#echo $if_tries_counter101 > /scripts_hvv/sys_routines/if_tries_count101
-#fi
-#fi
+let "if_tries_counter101=if_tries_counter101+1"
+echo $if_tries_counter101 > /scripts_hvv/sys_routines/if_tries_count101
+fi
+fi
