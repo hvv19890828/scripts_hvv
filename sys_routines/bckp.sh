ee73b41ce0f3c0a1af2ec8bd130b8779a512328a
Viacheslav Hudzovskyi <hvv19890828@gmail.com>
2023-03-25 02:09:13 +0200

    Added SleepOnACOutage mode

--- sys_routines/bckp.sh
+++ sys_routines/bckp.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #vars initialization
 stateofbckp_hvv=$(lsblk | grep bckp_hvv | wc -l)
+stateofAC=$(cat /sys/class/power_supply/ACAD/online)
 #verification
+if (( $stateofAC == 1 ));
+then
 if (( $stateofbckp_hvv == 1 ));
 then
 chmod -R 777 /home/hvv/Downloads/
 chmod -R 777 /bckp_hvv/Downloads/
 rsync -a --delete /home/hvv/Downloads/ /bckp_hvv/Downloads
 fi
+fi
+
