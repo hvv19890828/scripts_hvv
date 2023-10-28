92d2ea4b49040e09027d88b2ed5b65596b5e9a10
hvv19890828 <36333626+hvv19890828@users.noreply.github.com>
2023-10-28 18:44:13 +0300

    Update common-setup.sh

--- vmprov/vagrant/ubuntu/common-setup.sh
+++ vmprov/vagrant/ubuntu/common-setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo timedatectl set-timezone Europe/Kiev
 sudo chmod 777 /etc/ssh/sshd_config
 sudo sed 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config > tmp
 sudo cat tmp > /etc/ssh/sshd_config
