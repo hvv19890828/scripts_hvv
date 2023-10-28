9d2151f01ed83f99775a3a4eac1d5b9084ff99e2
hvv19890828 <36333626+hvv19890828@users.noreply.github.com>
2023-10-28 18:27:25 +0300

    Update common-setup.sh

--- vmprov/vagrant/ubuntu/common-setup.sh
+++ vmprov/vagrant/ubuntu/common-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo timedatectl set-timezone Europe/Kiev
 sudo chmod 777 /etc/ssh/sshd_config
 sudo sed 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config > tmp
 sudo cat tmp > /etc/ssh/sshd_config
@@ -6,4 +7,3 @@ sudo sed 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/ss
 sudo cat tmp > /etc/ssh/sshd_config
 sudo service ssh restart
 echo -e "user1!\nuser1!" | sudo passwd root
-sudo timedatectl set-timezone Europe/Kiev
